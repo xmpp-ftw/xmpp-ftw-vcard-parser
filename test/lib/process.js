6f71498e6b37e9b653caa1ce42253213fbb3aa3e
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-07-30 17:58:35 +0100

    Add test for no output format

--- test/lib/process.js
+++ test/lib/process.js
@@ -1,11 +1,15 @@
 'use strict';
 
+var ltx = require('ltx')
+
 require('should')
 
 var processVCard = require('../../index')
 
 /* jshint -W030 */
 describe('Checking input format', function() {
+    
+    var vCardTemp = new ltx.parse('<iq><vCard xmlns="vcard-temp"/></iq>')
     
     describe('Input format not supplied', function() {
         
@@ -18,6 +22,15 @@ describe('Checking input format', function() {
             }
         })
         
+    })
+    
+    it('Throws error if output format not requested', function(done) {
+        try {
+            processVCard(vCardTemp)
+        } catch (e) {
+            e.message.should.equal('No output format supplied')
+            done()
+        }
     })
     
 })
